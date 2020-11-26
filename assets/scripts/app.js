373ce19700fcda20771c375aa2f5d86abc4bf9df
mic <micmor.m@gmail.com>
2020-11-26 13:55:00 -0500

    update app from object literal construction to class definition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,40 @@
+//create class
+//first letter of each word in the name have to be capital
+//assignment of a key dne with = not :
+//at the end of each row ; not ,
+class Product {
+  title = "DEFAULT";
+  imageUrl;
+  price;
+  description;
+
+  //special metod execute everytime a new object is created
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
 //as OOP intead than create an event listener global
 //we will work inside an object
 const productList = {
   //list of products
   products: [
-    {
-      title: "A pillow",
-      imageUrl:
-        "https://www.publicdomainpictures.net/pictures/250000/velka/decorative-bed-pillows.jpg",
-      price: "19.99",
-      description: "A soft pillow!",
-    },
-    {
-      title: "A carpet",
-      imageUrl:
-        "https://www.publicdomainpictures.net/pictures/60000/velka/carpet-background.jpg",
-      price: "89.99",
-      description: "A nice carpet!",
-    },
+    //call the Class Product
+    new Product(
+      "A pillow",
+      "https://www.publicdomainpictures.net/pictures/250000/velka/decorative-bed-pillows.jpg",
+      "A soft pillow!",
+      "19.99"
+    ),
+    new Product(
+      "A carpet",
+      "https://www.publicdomainpictures.net/pictures/60000/velka/carpet-background.jpg",
+      "A nice carpet!",
+      "89.99"
+    ),
   ],
 
   //render METHOD written in short-hand notation (instead than render: function () {...})
