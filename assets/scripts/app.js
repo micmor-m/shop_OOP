2c1f7649fef43d40dcdad6ba08775375f16c5fa0
mic <micmor.m@gmail.com>
2020-11-25 21:04:33 -0500

    add productList object with render method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,6 @@
-
-
 //as OOP intead than create an event listener global
 //we will work inside an object
 const productList = {
-  
   //list of products
   products: [
     {
@@ -23,5 +20,33 @@ const productList = {
   ],
 
   //render METHOD written in short-hand notation (instead than render: function () {...})
-  render()
-}
+  render() {
+    //1 create a referent to the area where I want add the data
+    const renderHook = document.getElementById("app");
+    //2 create the element to insert
+    const prodList = document.createElement("ul");
+    //3 add style class to new element
+    prodList.className = "products-list";
+
+    //4 create the logic to render a single product in the list
+    for (const product of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+      <div>
+        <img src="${product.imageUrl}" alt="${product.title}" ></img>
+        <div class="product-item__content">
+          <h2>${product.title}</h2>
+          <h3>\$${product.price}</h3>
+          <p>${product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
+};
+
+productList.render();
